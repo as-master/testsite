b7670f9436a6a6749225dd1eedf5fefb9dfe8c0b
as-master <pliivaste@gmail.com>
2022-05-04 07:38:21 +0700

    Initial commit

---
+++ mysite/news/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import News
+# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at','updated_at','is_published',)
+    list_display_links = ('id', 'title', )
+    search_fields = ('title', )
+
+
+admin.site.register(News, NewsAdmin)
---
+++ mysite/news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Наименование')
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
